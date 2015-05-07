56842271a05f33557eb7106cc7b11d0f67fdfea9
Nick Kornek <nkornek@gmail.com>
2015-05-07 11:09:49 -0400

    Main Control

---
+++ Assets/MainControl.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainControl : MonoBehaviour {
+    public ChrisAV chrisAV;
+    public KaraAudio karaAudio;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void PlayChrisVideo(ChrisAV.whichVideo video)
+    {
+        chrisAV.SetVideo(video);
+    }
+
+    public void PlayChrisAudio(ChrisAV.whichSound sound)
+    {
+        chrisAV.PlayASound(sound);
+    }
+
+    public void PlayKaraAudio(KaraAudio.whichSound sound)
+    {
+        karaAudio.PlayASound(sound);
+    }
+}
