f64815b77cb348c369d109ebbfe2d2a85f8787e5
Nick Kornek <nkornek@gmail.com>
2015-05-07 15:06:47 -0400

    rotation script

---
+++ Assets/junkRotate.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class junkRotate : MonoBehaviour {
+    public bool rotate;
+    public Vector3 turnSpeed;
+    Vector3 currentAngle;
+
+
+	// Use this for initialization
+	void Start () {
+        currentAngle = new Vector3 (transform.localRotation.x, transform.localRotation.y, transform.localRotation.z);
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (rotate)
+        {
+            currentAngle += turnSpeed * Time.deltaTime;
+            transform.localRotation = Quaternion.Euler(currentAngle);
+        }
+	
+	}
+}
