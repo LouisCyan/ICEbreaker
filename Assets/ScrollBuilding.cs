200a8188f56d06f7b857647f956d6689cf55453f
Nick Kornek <nkornek@gmail.com>
2015-05-06 20:14:43 -0400

    Scrolling Texture script

---
+++ Assets/ScrollBuilding.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollBuilding : MonoBehaviour {
+    
+    protected Material textureToAnimate;
+
+    protected Vector2 uvOffset = Vector2.zero;
+    public Vector2 uvAnimationRate;
+    public string textureName = "_MainTex";
+
+    protected MeshRenderer backgroundMeshRenderer;
+
+    [SerializeField]
+    protected bool resetPositionToZero = true;
+
+    protected void Start()
+    {
+        backgroundMeshRenderer = GetComponent<MeshRenderer>();
+
+        if (backgroundMeshRenderer != null)
+        {
+            if (resetPositionToZero)
+                backgroundMeshRenderer.transform.position = Vector3.zero;
+
+            textureToAnimate = backgroundMeshRenderer.material;
+        }
+        float startOffset = Random.Range(0, 1);
+    }
+
+    protected void Update()
+    {
+        if (textureToAnimate != null)
+        {
+            if (uvOffset.x >= 1.0f)
+            {
+                uvOffset.x = 0.0f;
+            }
+
+            if (uvOffset.y >= 1.0f)
+            {
+                uvOffset.y = 0.0f;
+            }
+
+            uvOffset += uvAnimationRate * Time.deltaTime;
+            textureToAnimate.mainTextureOffset = uvOffset;
+        }
+    }
+}
